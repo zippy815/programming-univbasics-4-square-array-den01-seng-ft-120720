63a5006116d4c2bdebcf187735ab382c2eb1d9d6
Zipporah Horch <zipporahhorch@gmail.com>
2020-11-05 00:31:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   new_array.length.times do |numbers|
-    new_array.push(numbers * numbers)
+    new_array.push(array[numbers] * numbers)
 end
 return new_array
 end
