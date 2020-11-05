6d238fabc269e6c2dcf07010cee1ea55c7caa355
Zipporah Horch <zipporahhorch@gmail.com>
2020-11-05 00:43:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,3 +2,11 @@ def square_array(array)
     numbers=numbers * numbers
     square_array << numbers
 end
+def square_array(array)
+  # your code here	  square_array = []
+
+  array.each do |number|
+    square_array << number * number
+  end
+
+  square_array
