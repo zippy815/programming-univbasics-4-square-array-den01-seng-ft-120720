f6833bfad5921df9bae7ef5b10a82f728aef741a
Zipporah Horch <zipporahhorch@gmail.com>
2020-11-05 00:39:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   array.length.times do |numbers|
     numbers=numbers**2
-    array= new_array.push
+    array= new_array.push(array[numbers])
 end
 return array
 end
